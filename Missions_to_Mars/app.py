a235f15fdf7f55c3f0af9f95b17a2fa50fcec627
Jordan Roessle <screamaimfire@me.com>
2021-06-10 17:54:39 -0700

    app.py should be functional, index.html created

---
+++ Missions_to_Mars/app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+import mission_to_mars
+
+# initialize flask
+app = Flask(__name__)
+
+# initalize mongo connection with pymongo
+mongo = PyMongo(app, uri='mongodb://localhost:27017/mars')
+
+@app.route("/")
+def home():
+	# grab data from mongo database
+	scraped_data = mongo.db.collection.find_one()
+
+	# return template and data
+	return render_template("index.html", scraped_data = scraped_data)
+
+
+@app.route("/scrape")
+def scrape_information():
+	# run scrape function
+	mars_data = mission_to_mars.scrape()
+
+	# update Mongo database 
+	mongo.db.collection.update({}, mars_data, upsert=True)
+
+	# return redirect to home page
+	return redirect("/")
+
+if __name__ == "__main__"
+	app.run(debug=True)	
--- Missions_to_Mars/mission_to_mars.py
+++ Missions_to_Mars/mission_to_mars.py
@@ -3,118 +3,75 @@ from bs4 import BeautifulSoup
 from splinter import Browser
 from webdriver_manager.chrome import ChromeDriverManager
 
+def scrape():
+    # browser setup
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=False)
 
-# In[2]:
-
-
-executable_path = {'executable_path': ChromeDriverManager().install()}
-
-
-# In[3]:
-
-
-browser = Browser('chrome', **executable_path, headless=False)
-url = "https://redplanetscience.com/"
-
-browser.visit(url)
-soup = BeautifulSoup(browser.html, 'html.parser')
-browser.quit()
-
-
-# In[4]:
-
-
-news_title = soup.find('div', class_ = 'content_title').text
-news_title
-
-
-# In[5]:
-
-
-news_p = soup.find('div', class_ = 'article_teaser_body').text
-news_p
-
-
-# In[6]:
-
-
-browser = Browser('chrome', **executable_path, headless=False)
-url = "https://spaceimages-mars.com/"
-
-browser.visit(url)
-soup = BeautifulSoup(browser.html, "html.parser")
-browser.quit()
-
-
-# In[7]:
-
-
-featured_image_url = soup.find("img", class_="headerimage fade-in")
-featured_image_url = url + featured_image_url['src']
-featured_image_url
-
-
-# In[8]:
-
-
-url = "https://galaxyfacts-mars.com/"
-tables = pd.read_html(url)
-df = tables[0]
-df
-
-
-# In[9]:
-
-
-df = df.iloc[1:]
-df.columns = ["Desciption", "Mars", "Earth"]
-df.reset_index(drop=True, inplace=True)
-df
-
-
-# In[10]:
-
-
-html_table = df.to_html("MarsVsEarth.html")
-
-
-# In[11]:
-
-
-browser = Browser('chrome', **executable_path, headless=False)
-url = "https://marshemispheres.com/"
-
-
-# In[12]:
-
-
-hemisphere_image_urls = []
-
-
-# In[13]:
-
-
-for i in range(0,4):
+    # beautiful soup to grab html code from first webpage
+    url = "https://redplanetscience.com/"
     browser.visit(url)
-    browser.links.find_by_partial_text('Hemisphere Enhanced')[i].click()
+    soup = BeautifulSoup(browser.html, 'html.parser')
+    browser.quit()
+
+    # grab the newest news title and paragraph 
+    news_title = soup.find('div', class_ = 'content_title').text
+    news_p = soup.find('div', class_ = 'article_teaser_body').text
+
+    # browser for next url, beautiful soup to grab html
+    browser = Browser('chrome', **executable_path, headless=False)
+    url = "https://spaceimages-mars.com/"
+    browser.visit(url)
     soup = BeautifulSoup(browser.html, "html.parser")
-    title = soup.find("h2", class_="title").text
-    img = soup.find("img", class_="wide-image")['src']
-    hemisphere_image_urls.append({
-                    'title':title, 
-                    'img_url':url + img
-    })
-browser.quit()    
+    browser.quit()
 
+    # grab desired image
+    featured_image_url = soup.find("img", class_="headerimage fade-in")
+    featured_image_url = url + featured_image_url['src']
 
-# In[14]:
+    # use pandas read_html to grab table from the next url
+    url = "https://galaxyfacts-mars.com/"
+    tables = pd.read_html(url)
+    df = tables[0]
 
+    # cleaning the table, removing first line and renaming columns
+    df = df.iloc[1:]
+    df.columns = ["Desciption", "Mars", "Earth"]
+    df.reset_index(drop=True, inplace=True)
 
-hemisphere_image_urls
+    # exporting html code for table after cleaning 
+    df.to_html("MarsVsEarth.html")
 
+    # browser for next url,
+    browser = Browser('chrome', **executable_path, headless=False)
+    url = "https://marshemispheres.com/"
 
-# In[ ]:
+    # empty list for image paths to be stored in later
+    hemisphere_image_urls = []
 
+    # loop through hemisphere enhanced web pages 
+    for i in range(0,4):
+        browser.visit(url)
+        browser.links.find_by_partial_text('Hemisphere Enhanced')[i].click()
 
+        # use beautiful soup to grab desired image paths and titles
+        soup = BeautifulSoup(browser.html, "html.parser")
+        title = soup.find("h2", class_="title").text
+        img = soup.find("img", class_="wide-image")['src']
 
+        # append to list
+        hemisphere_image_urls.append({
+                        'title':title, 
+                        'img_url':url + img
+        })
+    browser.quit()
 
+    print(html_table)
+    return export_dic = {
+        'news_title':news_title,
+        'news_p':news_p, 
+        'mars_surface':featured_image_url,
+        'html_file':"MarsVsEarth.html",
+        'hemisphere_images':hemisphere_image_urls
+    }
+scrape()
